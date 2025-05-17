898db0ddf95533c11baf0db834946b55fcbeb4b6
Madhugit15 <madhushree1530@gmail.com>
2025-05-17 16:59:35 +0530

    new files are added

---
+++ README.md
@@ -0,0 +1,13 @@
+## About this Package
+
+It generates random number between 1 to 100
+
+## Installation
+
+Run `npm i madhu_random_number_generator`
+
+use:
+
+import RanNumGen from "madhu_random_number_generator"
+
+RanNumGen(3,20)
---
+++ src/app.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [color, setColor] = useState('#ff0000'); // Default color: red
+
+  const handleColorChange = (e) => {
+    setColor(e.target.value);
+  };
+
+  return (
+    <div className="App">
+      <h1>React Color Picker</h1>
+      <input
+        type="color"
+        value={color}
+        onChange={handleColorChange}
+        style={{ width: '50px', height: '50px' }}
+      />
+      <p>Selected Color: {color}</p>
+      <div
+        style={{
+          width: '100px',
+          height: '100px',
+          backgroundColor: color,
+          marginTop: '20px',
+        }}
+      ></div>
+    </div>
+  );
+}
+
+export default App;
